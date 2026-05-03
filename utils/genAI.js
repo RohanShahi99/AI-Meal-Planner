bf58a394b1724bc2b94aaa139afe58872d6f0593
Rohan Shahi <rohandybala99@gmail.com>
2026-05-03 04:28:49 -0700

    try gemini-1.5-pro

--- utils/genAI.js
+++ utils/genAI.js
@@ -4,7 +4,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function generateText({ prompt }) {
   try {
-    const modelInstance = genAI.getGenerativeModel({ model: 'gemini-1.0-pro' });
+    const modelInstance = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
     const result = await modelInstance.generateContent(prompt);
     return result;
   } catch (error) {
