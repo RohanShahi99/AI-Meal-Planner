8b9893521483ad515392c92185496095bf71146b
Rohan Shahi <rohandybala99@gmail.com>
2026-05-03 02:37:47 -0700

    update with fixed code

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://ai-meal-planner-jp94.onrender.com/api";
 
 // ================= AUTH =================
 function saveToken(token) {
